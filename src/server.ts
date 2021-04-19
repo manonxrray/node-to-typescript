274b506057bee9fa2d1a18c2fd178bbfbc8ed1f6
wazo <manon.gerray@gmail.com>
2021-04-19 15:10:31 +0200

    :dizzy: Error management with MongoError

--- src/server.ts
+++ src/server.ts
@@ -18,21 +18,34 @@ mongoose
   .then(() => console.log("Connected to database"))
   .catch((err: any) => console.log(err));
 
+// Mongo Errors 
+interface MongoError extends Error {
+  code: number;
+}
+
+function isMongoError(error: Error): error is MongoError {
+  return error.name === 'MongoError';
+}
+
+app.use((error: Error, req: Request, res: Response, _next: NextFunction) => {
+  if (isMongoError(error)) {
+    switch (error.code) {
+      case 11000:
+        res.status(400);
+        res.json({ success: false, message: 'The name is already used' });
+        break;
+      default:
+        res.status(400);
+        res.json({ success: false, message: 'An error occured' });
+    }
+  }
+});
+
 // Different uses
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
-
-// Typescript introduction
-
-app.use((error: any, req: Request, res: Response, next: NextFunction) => {
-  if (error.name === "MongoError" && error.code === 11000) {
-    res.status(400);
-    res.json({ success: false, message: "The name is already used" });
-  }
-})
-
 
 // Different requests
 
